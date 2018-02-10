e794fa3218f71bbb0ed8ff9e701bf2dc8f9e41cf
Luckiler <luckiler3@gmail.com>
2018-02-10 18:52:53 +0100

    Moved GUI scripts to own file

--- scripts/script.js
+++ scripts/script.js
@@ -56,8 +56,8 @@ function loadImage(input) {
             $(originalPhoto.photo).one('load', function() {
                 analysePhoto();
                 resize();
-                recomendSizeGUI();
-                updateGUI();
+                GUI.recommendSize();
+                GUI.update();
             });
         };
         fr.readAsDataURL(input.files[0]);
@@ -81,134 +81,6 @@ function analysePhoto() {
 
     originalPhoto.maxHeight = Math.round((originalPhoto.photo.naturalHeight * INCH) / DPI);
     originalPhoto.maxWidth = Math.round((originalPhoto.photo.naturalWidth * INCH) / DPI);
-}
-
-function updateGUI() {
-
-    setLayout('N', 0);
-
-    $('#maxSize').text(originalPhoto.maxWidth + 'x' + originalPhoto.maxHeight + 'mm');
-
-    if ((originalPhoto.maxWidth / 300 - 1) < 1) {
-        $('#dipV').css('display', 'none');
-        $('#triV').css('display', 'none');
-    } else if ((originalPhoto.maxWidth / 300 - 1) < 2) {
-        $('#dipV').css('display', 'inline-block');
-        $('#triV').css('display', 'none');
-    } else {
-        $('#dipV').css('display', 'inline-block');
-        $('#triV').css('display', 'inline-block');
-    }
-
-    if ((originalPhoto.maxHeight / 300 - 1) < 1) {
-        $('#dipH').css('display', 'none');
-        $('#triH').css('display', 'none');
-    } else if ((originalPhoto.maxHeight / 300 - 1) < 2) {
-        $('#dipH').css('display', 'inline-block');
-        $('#triH').css('display', 'none');
-    } else {
-        $('#dipH').css('display', 'inline-block');
-        $('#triH').css('display', 'inline-block');
-    }
-
-    var $formatText = $('#format');
-    switch (originalPhoto.closestRatio) {
-        case 1:
-            $formatText.text('Quadrado');
-            break;
-        case 3 / 2:
-            $formatText.text('Retangular');
-            break;
-        case 2:
-            $formatText.text('Semi Panoramico');
-            break;
-        case 3:
-            $formatText.text('Panoramico');
-            break;
-        case 2 / 3:
-            $formatText.text('Retrato');
-            break;
-    }
-
-
-    $('.formatButton').removeClass('w3-card-4').css("transform", "scale(1)");
-    var selector = '#f' + preview.layout.width + '' + preview.layout.height;
-    $(selector).addClass('w3-card-4').css("transform", "scale(1.05)");
-}
-
-function recomendSizeGUI() {
-    $rz = $('#recommendedSizes');
-    $rz.empty();
-
-    for (var s of ['EDIÇAO ESPECIAL', 'P', 'M', 'G', 'GG', 'COLECIONADOR']) {
-        var color, w, h, display;
-        display = true;
-
-        switch (s) {
-            case 'EDIÇAO ESPECIAL':
-                h = 300;
-                break;
-            case 'P':
-                h = 450;
-                break;
-            case 'M':
-                h = 600;
-                break;
-            case 'G':
-                if (originalPhoto.closestRatio === 2 / 3 || originalPhoto.closestRatio === 3 / 2)
-                    h = 800;
-                else if (originalPhoto.closestRatio === 1 / 3 || originalPhoto.closestRatio === 3)
-                    display = false;
-                else
-                    h = 900;
-                break;
-            case 'GG':
-                if (originalPhoto.closestRatio !== 2 / 3 || originalPhoto.closestRatio !== 3 / 2)
-                    display = false;
-                h = 1000;
-                break;
-            case 'COLECIONADOR':
-                if (originalPhoto.closestRatio === 2 / 3 || originalPhoto.closestRatio === 3 / 2 || originalPhoto.closestRatio === 1)
-                    h = 1200;
-                else if (originalPhoto.closestRatio === 1 / 2 || originalPhoto.closestRatio === 2)
-                    h = 1050;
-                else if (originalPhoto.closestRatio === 1 / 3 || originalPhoto.closestRatio === 3)
-                    h = 700;
-                break;
-        }
-
-        if (display) {
-            var text;
-            var funcValues;
-            var idValues;
-            if (originalPhoto.closestRatio >= 1) {
-                w = h * originalPhoto.closestRatio;
-                if (originalPhoto.maxHeight >= h && originalPhoto.maxWidth >= w) {
-                    color = 'green';
-                    setFormat(w, h);
-                } else if (originalPhoto.maxHeight >= h || originalPhoto.maxWidth >= w)
-                    color = 'orange';
-                else
-                    color = 'red';
-                text = w / 10 + 'x' + h / 10 + 'cm';
-                funcValues = w + ',' + h;
-                idValues = w + '' + h;
-            } else {
-                w = h / originalPhoto.closestRatio;
-                if (originalPhoto.maxWidth >= h && originalPhoto.maxHeight >= w) {
-                    color = 'green';
-                    setFormat(h, w);
-                } else if (originalPhoto.maxWidth >= h || originalPhoto.maxHeight >= w)
-                    color = 'orange';
-                else
-                    color = 'red';
-                text = h / 10 + 'x' + w / 10 + 'cm';
-                funcValues = h + ',' + w;
-                idValues = h + '' + w;
-            }
-            $rz.append('<div class = "w3-container w3-margin w3-' + color + ' w3-border-theme formatButton" id="f' + idValues + '" onclick="selectFormat(' + funcValues + ')"><p><b>' + s + '</b></p><p>' + text + '</div>');
-        }
-    }
 }
 
 // Resizes the photo for faster preview rendering
